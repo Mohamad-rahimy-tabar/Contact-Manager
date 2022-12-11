40fc526359a2edc6ecbbc24a715e9ca84c32a59e
mohamad.r.t <mohamad.r.t.2314@gmail.com>
2022-12-11 23:21:29 +0330

    finalstep

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -14,10 +14,10 @@ const contextClass = {
 
 const Layout = ({ children }) => {
   return (
-    <div className="h-screen w-screen flex flex-col gap-4 items-center fixed top-0 left-0 ">
+    <div className="h-screen w-screen flex flex-col gap-4 items-center  fixed top-0 left-0 ">
       <div className="container p-2 w-full  h-[calc(100%-60px)] md:h-full">
         <ToastContainer
-          toastClassName={({ type }) => contextClass[type || "default"] + " relative flex flex-nowrap p-2 md:w-[2000px] w-[210px]  rounded-md w-auto justify-between overflow-hidden cursor-pointer"}
+          toastClassName={({ type }) => contextClass[type || "default"] + " relative flex flex-nowrap p-2 md:max-w-[2000px] max-w-[180px]  rounded-md justify-between overflow-hidden cursor-pointer"}
           bodyClassName={() => "text-[8px] md:text-sm font-light flex flex-nowrap justify-between items-center font-white font-sans block p-2"}
           position="top-left"
           autoClose={3000}
